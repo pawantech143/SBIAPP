7aa2e3e3c7ba96e01f0eaf62f6150b45bf083ce9
pawan-pc <pawan@gmail>
2020-08-20 20:33:06 +0530

    bug fixed

---
+++ Loan.java
@@ -0,0 +1,22 @@
+Class Loan{
+
+    public void doPayment(){
+
+
+                           }
+
+
+
+ public void doPay(){
+
+
+                           }
+
+
+public void doPay123(){
+
+
+                           }
+
+
+}
