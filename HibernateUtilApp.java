598a301564e15902d355481346c6a2b686fb345d
pawantech143 <69728247+pawantech143@users.noreply.github.com>
2020-08-20 12:56:53 +0530

    Add files via upload
    first commit

---
+++ HibernateUtilApp.java
@@ -0,0 +1,17 @@
+package com.pawan.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtilApp {
+
+	private static SessionFactory sf = null;
+	static {
+		sf = new Configuration().configure().buildSessionFactory();
+	}
+
+	public static SessionFactory getSessionFactory() {
+
+		return sf;
+	}
+}
